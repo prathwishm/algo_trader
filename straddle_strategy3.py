1cad736c33f0a1a0358f1514a74e8fafd157f709
Prasad <prasad.mestha@tesco.com>
2022-06-23 20:53:46 +0530

    Fix issues

--- straddle_strategy3.py
+++ straddle_strategy3.py
@@ -12,6 +12,13 @@ file_handler = logging.FileHandler('straddle_strategy3_error.log')
 file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s'))
 logger.addHandler(file_handler)
 
+NF_LOT_SIZE = 50
+BNF_LOT_SIZE = 25
+
+CORE_STRATEGY_LOT_MULTIPLIER = 2
+EXPIRY_STRATEGY_LOT_MULTIPLIER = 1
+OTHER_STRATEGY_LOT_MULTIPLIER = 1
+
 MONDAY = 1
 TUESDAY = 2
 WEDNESDAY = 3
@@ -38,8 +45,8 @@ trades_list = [
     {
         'strategy_name': '9_16_strangle',
         'strategy_type': 'add_nf_strangle_to_watchlist',
-        'entry_time': [9, 16, 0],
-        'quantity': 100,
+        'entry_time': [20, 16, 0],
+        'quantity': NF_LOT_SIZE * CORE_STRATEGY_LOT_MULTIPLIER,
         'execution_days': [{
             'day': MONDAY,
             'exit_time': [15, 15, 4],
@@ -59,7 +66,7 @@ trades_list = [
         'strategy_type': 'short_bnf_straddle',
         'entry_time': [9, 16, 0],
         'exit_time': [11, 44, 4],
-        'quantity': 25,
+        'quantity': BNF_LOT_SIZE * EXPIRY_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.25,
         'strangle': True,
         'strike_distance': 100,
@@ -76,7 +83,7 @@ trades_list = [
         'strategy_name': 'nf_9_16_expiry_strangle',
         'strategy_type': 'short_nifty_straddle',
         'entry_time': [9, 16, 0],
-        'quantity': 50,
+        'quantity': NF_LOT_SIZE * EXPIRY_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.2,
         'strangle': False,
         'execution_days': [{
@@ -89,7 +96,7 @@ trades_list = [
         'strategy_name': '9_20_straddle',
         'strategy_type': 'add_bnf_straddle_to_watchlist',
         'entry_time': [9, 19, 54],
-        'quantity': 50,
+        'quantity': BNF_LOT_SIZE * CORE_STRATEGY_LOT_MULTIPLIER,
         'execution_days': [{
             'day': MONDAY,
             'exit_time': [14, 54, 54],
@@ -116,7 +123,7 @@ trades_list = [
         'strategy_name': 'nf_9_40_strangle',
         'strategy_type': 'short_nifty_straddle',
         'entry_time': [9, 39, 52],
-        'quantity': 50,
+        'quantity': NF_LOT_SIZE * OTHER_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.35,
         'strangle': True,
         'strike_distance': 100,
@@ -146,7 +153,7 @@ trades_list = [
         'strategy_name': '10_05_strangle',
         'strategy_type': 'add_bnf_strangle_to_watchlist',
         'entry_time': [10, 5, 0],
-        'quantity': 25,
+        'quantity': BNF_LOT_SIZE * OTHER_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.2,
         'execution_days': [{
             'day': MONDAY,
@@ -174,7 +181,7 @@ trades_list = [
         'strategy_name': 'nf_10_45_strangle',
         'strategy_type': 'short_nifty_straddle',
         'entry_time': [10, 44, 52],
-        'quantity': 100,
+        'quantity': NF_LOT_SIZE * CORE_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.28,
         'strangle': True,
         'strike_distance': 50,
@@ -204,7 +211,7 @@ trades_list = [
         'strategy_name': '11_15_strangle',
         'strategy_type': 'add_bnf_strangle_to_watchlist',
         'entry_time': [11, 15, 0],
-        'quantity': 25,
+        'quantity': BNF_LOT_SIZE * OTHER_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.18,
         'execution_days': [{
             'day': MONDAY,
@@ -232,12 +239,28 @@ trades_list = [
         'strategy_name': 'nf_11_30_strangle',
         'strategy_type': 'short_nifty_straddle',
         'entry_time': [11, 29, 52],
-        'quantity': 50,
+        'quantity': NF_LOT_SIZE * OTHER_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.25,
         'strangle': True,
         'strike_distance': 50,
         'execution_days': [{
+            'day': MONDAY,
+            'exit_time': [15, 17, 4],
+            'hedge_multiplier': 1
+        },{
+            'day': TUESDAY,
+            'exit_time': [15, 17, 4],
+            'hedge_multiplier': 1
+        },{
+            'day': WEDNESDAY,
+            'exit_time': [15, 17, 4],
+            'hedge_multiplier': 1
+        },{
             'day': THRUSDAY,
+            'exit_time': [15, 17, 4],
+            'hedge_multiplier': 1
+        },{
+            'day': FRIDAY,
             'exit_time': [15, 17, 4],
             'hedge_multiplier': 1
         }],
@@ -246,7 +269,7 @@ trades_list = [
         'strategy_name': '11_45_strangle',
         'strategy_type': 'add_bnf_strangle_to_watchlist',
         'entry_time': [11, 44, 54],
-        'quantity': 50,
+        'quantity': BNF_LOT_SIZE * CORE_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.20,
         'execution_days': [{
             'day': MONDAY,
@@ -274,7 +297,7 @@ trades_list = [
         'strategy_name': '13_20_strangle',
         'strategy_type': 'add_bnf_strangle_to_watchlist',
         'entry_time': [13, 20, 0],
-        'quantity': 25,
+        'quantity': BNF_LOT_SIZE * OTHER_STRATEGY_LOT_MULTIPLIER,
         'sl_percent': 0.25,
         'execution_days': [{
             'day': MONDAY,
@@ -513,7 +536,7 @@ class straddles:
             self.add_straddle_to_websocket(bnf_atm_strike, index = 'BANKNIFTY')
 
         for trades_item in self.trades_list:
-            execution_day_details = filter(lambda execution_days: execution_days['day'] == self.iso_week_day, trades_list[0]['execution_days'])
+            execution_day_details = [execution_days for execution_days in trades_item['execution_days'] if execution_days['day'] == self.iso_week_day]
             if len(execution_day_details) > 0:
                 execution_day_details = execution_day_details[0]
                 if trades_item['strategy_name'] not in self.trades_placed and current_dt.hour == trades_item['entry_time'][0] and current_dt.minute >= trades_item['entry_time'][1] and (current_dt.second >= trades_item['entry_time'][2] or current_dt.minute > trades_item['entry_time'][1]):
